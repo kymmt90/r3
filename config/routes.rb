5b994d15be7a6dd07a438e01ab7228956dcbbef1
Kohei Yamamoto <kymmt90@gmail.com>
2016-06-16 22:15:18 +0900

    Refactor config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,21 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'login' => 'sessions#new'
+
+  get  'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
-  resources :users, except: [:index] do
-    resources :subscriptions, only: [:index, :new, :create, :destroy]
-    resources :feed_categorizations, only: [:index, :new, :create, :destroy]
-  end
+
+  resources :categories, except: [:index, :show]
+
   resources :feeds, only: [:show, :create, :destroy] do
     resources :entries, only: [:show]
   end
   get 'feeds/refresh/:id' => 'feeds#refresh', as: 'refresh_feed'
-  resources :categories, only: [:new, :create, :edit, :update, :destroy]
+
   resources :reading_statuses, only: [:update]
+
+  resources :users, except: [:index] do
+    resources :feed_categorizations, except: [:edit, :update]
+    resources :subscriptions, except: [:edit, :update]
+  end
 end
