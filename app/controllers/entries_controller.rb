96a0b242eb39f9d50e529266e1760e6d61069ab5
Kohei Yamamoto <kymmt90@gmail.com>
2016-06-26 23:02:58 +0900

    Refactor views

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -5,8 +5,8 @@ class EntriesController < ApplicationController
     @feed = Feed.find(params[:feed_id])
     @entry = @feed.entries.find(params[:id])
     if current_user.subscribe?(@feed)
-      status = @entry.reading_statuses.find_by(user_id: current_user.id)
-      status.update_attributes(status: :read)
+      @status = @entry.reading_statuses.find_by(user_id: current_user.id)
+      @status.update_attributes(status: :read)
     end
   end
 end
